98dc5f48c8962014c4fa512b4985d931ae421bbc
iamkaushalkb <49513231+iamkaushalkb@users.noreply.github.com>
2020-05-03 03:54:31 -0500

    uiupdate

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+app.config[ 'SECRET_KEY' ] = 'key'
+socketio = SocketIO( app )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/smartmill'
+db = SQLAlchemy(app)
 
+class Message(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user = db.Column(db.String(12), nullable=False)
+  message = db.Column(db.String(255), nullable=False)
 
-@app.route('/')
-def sessions():
-    return render_template('session.html')
+@app.route( '/session' , methods = ['GET', 'POST'])
+def hello():
+      if(request.method=='POST'):
+        user = request.form.get('user')
+        msg = request.form.get('msg')
+        entry = Message( name=name, message=msg )
+        db.session.add(entry)
+        db.session.commit()
+      return render_template( 'session.html' )  
 
+def messageRecived():
+  print( 'message was received!!!' )
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'my response', json, callback=messageRecived )
 
 if __name__ == '__main__':
-	socketio.run(app, debug = True, use_reloader = False, port=1111)
+  socketio.run( app, debug = True )
